c4db57a00cd59ad83af835e603269cc290616082
master <kmg1151@naver.com>
2023-04-17 01:59:13 +0900

    주석 추가

--- socket/src/ch03/Client.java
+++ socket/src/ch03/Client.java
@@ -8,20 +8,27 @@ import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+// 소켓 통신을 하려면 소켓이 필요하다. (서버측 연결 담당 포트 번호 알아야 함.)
 public class Client {
 
+	// 자신의 ip 
 	final String IP = "localhost";
+	
 	Socket socket;
 	BufferedReader bufferedReader;
 	BufferedWriter bufferedWriter;
 
 	public Client() {
 		try {
+			// 소켓 생성과 동시에 ip, 연결 담당 포트 번호 
 			socket = new Socket(IP, 10002);
-
+			
+			// 키보드로 받은 내용을 읽을 예정
 			bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			// 출력을 위한 소켓의 기반 스트림 불러오기.(?)
 			bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			
+			// 이 부분에서 while(true)로 해서 무한 반복이 일어나 server측 코드가 flush()가 되지 않았었음.
 			boolean flag = true;
 			while (flag == true) {
 				String msg = bufferedReader.readLine();
--- socket/src/ch03/Server.java
+++ socket/src/ch03/Server.java
@@ -10,7 +10,9 @@ import java.net.Socket;
 
 public class Server {
 
+	// 연결만 주고 받을 서버 소켓 (연결만 하고 끊어짐.)
 	ServerSocket serverSocket;
+	// 실제 통신을 할 소켓 필요 
 	Socket socket;
 	
 	BufferedReader bufferedReader;
@@ -18,22 +20,25 @@ public class Server {
 	
 	public Server() {
 		try {
+			// 서버 소켓을 생성함과 동시에 연결할 포트 번호
 			serverSocket = new ServerSocket(10002);
 			socket = serverSocket.accept(); // 클라이언트와 연결 완료
 			
+			// 입력을 위한 socket의 기반스트림을 불러와야 함.(?)
 			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			bufferedWriter = new BufferedWriter(new FileWriter("c:/tmp/hw.txt"));
+			// mj.txt파일로 출력할 예정
+			bufferedWriter = new BufferedWriter(new FileWriter("c:/tmp/mj.txt"));
 			
+			// 문자열을 null일 때까지 읽음 
 			String str;
 			while((str = bufferedReader.readLine())!= null) {
-				System.out.println("여기까지 들어왔나요");
 				bufferedWriter.write(str);
 			}
-			System.out.println("hello");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}finally {
 			try {
+				// 물내려주고 닫음.
 				bufferedWriter.flush();
 				bufferedWriter.close();
 				bufferedReader.close();
